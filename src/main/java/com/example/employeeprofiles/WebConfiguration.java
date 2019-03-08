31864d924e106ad81868af08662f9ced4f0b10ea
Nathan Khoo <natebakescakes@gmail.com>
2019-03-08 15:41:58 +0800

    Fix CORS configuration

--- src/main/java/com/example/employeeprofiles/WebConfiguration.java
+++ src/main/java/com/example/employeeprofiles/WebConfiguration.java
@@ -2,15 +2,13 @@ package com.example.employeeprofiles;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://employee-profiles-client.herokuapp.com/");
+        registry.addMapping("/**").allowedOrigins("https://employee-profiles-client.herokuapp.com");
     }
 }
