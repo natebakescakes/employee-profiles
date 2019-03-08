31629ce8cdc276d13a92f3e69169e3ca1aad5fa9
Nathan Khoo <natebakescakes@gmail.com>
2019-03-08 15:23:46 +0800

    Allow client production url (#7)

--- src/main/java/com/example/employeeprofiles/EmployeeProfilesApplication.java
+++ src/main/java/com/example/employeeprofiles/EmployeeProfilesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeProfilesApplication {
@@ -12,5 +13,4 @@ public class EmployeeProfilesApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeProfilesApplication.class, args);
     }
-
 }
